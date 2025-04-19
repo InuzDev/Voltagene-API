d0ec8b9f378c476e99c73a26fd7fc6d70a5e0145
InuDeveloper <shiibadev@gmail.com>
2025-04-19 16:37:13 -0400

    Preparing for the new mailing service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContactModule } from './contact/contact.module';
-import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [ContactModule, MailerModule],
+  // imports: [ContactModule, MailerModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        }
+      ]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
